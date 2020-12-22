bbb4f6d571410d32278d65cf6df4d862c31e2372
Oleg Burundukov <oleg.burundukov@ing.com>
2020-12-22 10:39:23 +0100

    Minor changes
    Signed-off-by: Oleg Burundukov <oleg.burundukov@ing.com>

--- src/ecdsa/mod.rs
+++ src/ecdsa/mod.rs
@@ -24,6 +24,7 @@ use std::collections::BTreeSet;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 use std::sync::atomic;
+use trace::trace;
 use zeroize::Zeroize;
 
 pub mod keygen;
@@ -229,7 +230,10 @@ impl PaillierKeys {
         Paillier::decrypt(&self.dk, &RawCiphertext::from(c))
     }
 
+    /// checks whether Paillier's setup is valid and consistent
+    #[trace(pretty, prefix = "PaillierKeys::")]
     pub fn is_valid(ek: &EncryptionKey, dk: &DecryptionKey) -> bool {
+        // TODO : report back specific errors
         is_prime(&dk.p)
             && is_prime(&dk.q)
             && ek.n == dk.p.borrow() * dk.q.borrow()
